fff853dd37e6cd2731f511b154c5046845385a87
Carvin2016 <649629440@qq.com>
2016-04-25 11:36:07 +0800

    first commit

---
+++ githubSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  githubSample
+//
+//  Created by qf on 16/4/25.
+//  Copyright (c) 2016年 qf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
